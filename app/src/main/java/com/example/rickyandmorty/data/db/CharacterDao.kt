2f58dccc5ac4c3ad7d964d90e15958bb592f43c1
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2025-01-06 19:25:37 +0530

    Create Character Dao

---
+++ app/src/main/java/com/example/rickyandmorty/data/db/CharacterDao.kt
@@ -0,0 +1,22 @@
+package com.example.rickyandmorty.data.db
+
+import com.example.rickyandmorty.data.entity.Character
+import com.example.rickyandmorty.data.entity.CharacterResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CharacterDao {
+
+    @GET("character")
+    suspend fun getAllCharacters(@Query("page") page: Int): CharacterResponse
+
+    @GET("character")
+    suspend fun getFilteredCharacters(
+        @Query("name") name: String?,
+        @Query("status") status: String?
+    ): CharacterResponse
+
+    @GET("character/{id}")
+    suspend fun getCharacterById(@Query("id") id: Int?): Character
+}
+
