10d4872db3a42ac3c848e08c45bfaea316a19ac1
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2025-01-06 19:36:28 +0530

    Create Episode Dao

---
+++ app/src/main/java/com/example/rickyandmorty/data/db/EpisodeDao.kt
@@ -0,0 +1,24 @@
+package com.example.rickyandmorty.data.db
+
+import com.example.rickyandmorty.data.entity.Episode
+import com.example.rickyandmorty.data.entity.EpisodeResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface EpisodeDao {
+
+    @GET("episode")
+    suspend fun getAllEpisodes(@Query("pages") pages: Int): EpisodeResponse
+
+    @GET("episode")
+    suspend fun getFilteredEpisodes(
+        @Query("name") name: String? = null,
+        @Query("episode") episode: String? = null
+    ): EpisodeResponse
+
+    @GET("episode/{id}")
+    suspend fun getEpisodeById(@Query("id") id: Int): Episode
+
+    @GET("episode/{ids}")
+    suspend fun getEpisodeByIds(@Query("ids") ids: String): List<Episode>
+}
