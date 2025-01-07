28bf16f2f4866944fa70802a162d9ed56919a0c2
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2025-01-07 19:05:36 +0530

    Add EpisodeDataSource to manage episode data fetching

---
+++ app/src/main/java/com/example/rickyandmorty/data/datasource/EpisodeDataSource.kt
@@ -0,0 +1,39 @@
+package com.example.rickyandmorty.data.datasource
+
+import com.example.rickyandmorty.data.db.EpisodeDao
+import com.example.rickyandmorty.data.entity.Episode
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class EpisodeDataSource(private val episodeDao: EpisodeDao) {
+
+    suspend fun getAllEpisodes(): List<Episode> = withContext(Dispatchers.IO) {
+        val allEpisodes = mutableListOf<Episode>()
+        var page = 1
+        var hasNextPage: Boolean
+
+        do {
+            val response = episodeDao.getAllEpisodes(page)
+            allEpisodes.addAll(response.result)
+            hasNextPage = response.info.next != null
+            page++
+        } while (hasNextPage)
+
+        return@withContext allEpisodes
+    }
+
+    suspend fun getFilteredEpisodes(name: String? = null, episode: String? = null): List<Episode> =
+        withContext(Dispatchers.IO) {
+            val response = episodeDao.getFilteredEpisodes(name, episode)
+
+            return@withContext response.result
+        }
+
+    suspend fun getEpisodeById(id: Int): Episode = withContext(Dispatchers.IO) {
+        return@withContext episodeDao.getEpisodeById(id)
+    }
+
+    suspend fun getEpisodeByIds(ids: String): List<Episode> = withContext(Dispatchers.IO) {
+        return@withContext episodeDao.getEpisodeByIds(ids)
+    }
+}
