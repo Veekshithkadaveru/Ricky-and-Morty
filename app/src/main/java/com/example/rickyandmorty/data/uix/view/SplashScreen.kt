78a737f4685da9e6aab49724705a6ebc833594e5
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2025-01-19 19:37:09 +0530

    Create Splash screen

--- app/src/main/java/com/example/rickyandmorty/data/uix/view/SplashScreen.kt
+++ app/src/main/java/com/example/rickyandmorty/data/uix/view/SplashScreen.kt
@@ -1,2 +1,102 @@
 package com.example.rickyandmorty.data.uix.view
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.rickyandmorty.R
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController) {
+
+    val infiniteTransition = rememberInfiniteTransition(label = "")
+    val alphaAnim by infiniteTransition.animateFloat(
+        initialValue = 0.75f,
+        targetValue = 0.95f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 3000, easing = LinearEasing)
+        ), label = ""
+    )
+
+    val textScale = rememberInfiniteTransition(label = "")
+    val scaleAnim by textScale.animateFloat(
+        initialValue = 1f,
+        targetValue = 1.05f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1000, easing = FastOutSlowInEasing),
+            repeatMode = RepeatMode.Reverse
+        ), label = ""
+    )
+
+    LaunchedEffect(Unit) {
+        delay(5000)
+        navController.navigate("HomeScreen") {
+            popUpTo("SplashScreen") { inclusive = true }
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF121212)),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.img_rick_and_morty),
+                contentDescription = "Ricky and Morty",
+                modifier = Modifier
+                    .size(350.dp)
+                    .alpha(alphaAnim),
+                contentScale = ContentScale.Fit
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            BasicText(
+                text = "Ricky and Morty",
+                modifier = Modifier.scale(scaleAnim),
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF1F8A70),
+                    textAlign = TextAlign.Center,
+                    fontSize = 28.sp
+                )
+            )
+        }
+    }
+}
+
