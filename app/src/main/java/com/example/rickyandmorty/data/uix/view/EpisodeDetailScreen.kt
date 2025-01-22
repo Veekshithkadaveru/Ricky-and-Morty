2332aeb984e6a371b88bb78f6dcb6f0c94970155
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2025-01-22 20:29:02 +0530

    Add CharacterCard Composable to display character details

--- app/src/main/java/com/example/rickyandmorty/data/uix/view/EpisodeDetailScreen.kt
+++ app/src/main/java/com/example/rickyandmorty/data/uix/view/EpisodeDetailScreen.kt
@@ -1,16 +1,25 @@
 package com.example.rickyandmorty.data.uix.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -28,7 +37,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.rickyandmorty.data.entity.Character
 import com.example.rickyandmorty.data.uix.viewmodel.EpisodeDetailViewModel
+import com.skydoves.landscapist.glide.GlideImage
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -83,15 +94,79 @@ fun EpisodeDetailScreen(
                 )
                 Spacer(modifier = Modifier.height(16.dp))
 
-                Text(text = "Characters", color = Color(0xFF1F8A70), style = MaterialTheme.typography.titleMedium)
+                Text(
+                    text = "Characters",
+                    color = Color(0xFF1F8A70),
+                    style = MaterialTheme.typography.titleMedium
+                )
                 Spacer(modifier = Modifier.height(8.dp))
 
                 LazyColumn {
-                    items(characters){
-
+                    items(characters) {character->
+                        CharacterCard(character = character) {
+                            navController.navigate("CharacterDetailScreen/{${character.id}}")
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
                     }
                 }
             }
         }
     }
 }
+
+@Composable
+fun CharacterCard(character: Character, onClick: () -> Unit) {
+
+    val heartColor = when (character.status) {
+        "Alive" -> Color.Green
+        "Dead" -> Color.Red
+        else -> Color.Gray
+    }
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .clickable { onClick() }
+            .background(Color(0xFF202329)),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFF3C3E44))
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+
+                GlideImage(
+                    imageModel = character.image,
+                    contentDescription = character.name,
+                    modifier = Modifier.size(64.dp)
+                )
+
+                Spacer(modifier = Modifier.width(16.dp))
+                Column {
+                    Text(
+                        text = character.name,
+                        style = MaterialTheme.typography.titleMedium,
+                        color = Color(0xFF1F8A70)
+                    )
+                    Text(
+                        text = character.species,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = Color(0xFFD1D1D1)
+                    )
+                }
+            }
+            Icon(
+                imageVector = Icons.Default.Favorite,
+                contentDescription = "Heart Icon",
+                tint = heartColor,
+                modifier = Modifier.size(24.dp)
+            )
+        }
+    }
+}
+
