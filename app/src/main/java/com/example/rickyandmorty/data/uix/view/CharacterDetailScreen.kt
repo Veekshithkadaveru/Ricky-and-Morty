8af881ca9b55a21177f749d495da7b4952c6cb2e
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2025-01-23 19:45:30 +0530

    Add CharacterDetailContent Composable to display detailed character information

--- app/src/main/java/com/example/rickyandmorty/data/uix/view/CharacterDetailScreen.kt
+++ app/src/main/java/com/example/rickyandmorty/data/uix/view/CharacterDetailScreen.kt
@@ -4,15 +4,32 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Pets
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.Tv
+import androidx.compose.material.icons.filled.Wc
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -24,9 +41,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.rickyandmorty.data.entity.Character
+import com.example.rickyandmorty.data.uix.uicomponent.ShimmerEffect
 import com.example.rickyandmorty.data.uix.viewmodel.CharacterDetailViewModel
+import com.skydoves.landscapist.glide.GlideImage
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -50,7 +71,11 @@ fun CharacterDetailScreen(
                 title = { Text(text = "Character Details", color = Color(0xFF1F8A70)) },
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) {
-                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back", tint = Color.White)
+                        Icon(
+                            Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back",
+                            tint = Color.White
+                        )
                     }
                 },
                 modifier = Modifier.fillMaxWidth(),
@@ -69,13 +94,157 @@ fun CharacterDetailScreen(
         ) {
 
             character?.let {
-
+                CharacterDetailContent(character = it)
             } ?: Box(
                 modifier = Modifier.fillMaxSize(),
                 contentAlignment = Alignment.Center
             ) {
                 CircularProgressIndicator(color = Color(0xFF1F8A70))
             }
+        }
+    }
+}
+
+@Composable
+fun CharacterDetailContent(character: Character) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(bottom = 16.dp),
+            colors = CardDefaults.cardColors(containerColor = Color(0xFF202329)),
+            elevation = CardDefaults.cardElevation(8.dp)
+        ) {
+
+            GlideImage(
+                imageModel = character.image,
+                contentDescription = character.name,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f),
+                loading = {
+                    ShimmerEffect(modifier = Modifier.fillMaxSize())
+                }
+            )
+        }
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = "Character Icon",
+                tint = Color(0xFF1F8A70),
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = character.name,
+                style = MaterialTheme.typography.headlineMedium,
+                color = Color(0xFF1F8A70)
+            )
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                imageVector = Icons.Default.Favorite,
+                contentDescription = "Status Icon",
+                tint = when (character.status) {
+                    "Alive" -> Color.Green
+                    "Dead" -> Color.Red
+                    else -> Color.Gray
+                },
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "Status: ${character.status}",
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color(0xFFD1D1D1)
+            )
+        }
+        Spacer(modifier = Modifier.height(4.dp))
+
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                imageVector = Icons.Default.Pets,
+                contentDescription = "Species Icon",
+                tint = Color(0xFF1F8A70),
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "Species: ${character.species}",
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color(0xFFD1D1D1)
+            )
+        }
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                imageVector = Icons.Default.Wc,
+                contentDescription = "Gender Icon",
+                tint = Color(0xFF1F8A70),
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "Gender: ${character.gender}",
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color(0xFFD1D1D1)
+            )
+        }
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                imageVector = Icons.Default.LocationOn,
+                contentDescription = "Origin Icon",
+                tint = Color(0xFF1F8A70),
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "Origin: ${character.origin.name}",
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color(0xFFD1D1D1)
+            )
+        }
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                imageVector = Icons.Default.Place,
+                contentDescription = "Location Icon",
+                tint = Color(0xFF1F8A70),
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "Last known location: ${character.location.name}",
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color(0xFFD1D1D1)
+            )
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                imageVector = Icons.Default.Tv,
+                contentDescription = "Episodes Icon",
+                tint = Color(0xFF1F8A70),
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "Episodes: ${character.episode.joinToString { it.split("/").last() }}",
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color(0xFFD1D1D1)
+            )
         }
     }
 }
