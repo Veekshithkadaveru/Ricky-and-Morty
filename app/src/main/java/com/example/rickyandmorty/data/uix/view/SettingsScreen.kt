32103b71c680fa875850bcfb5e917eb00de09ffb
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2025-01-24 22:33:48 +0530

    Create SocialMediaIcon Composable for clickable social media icons

--- app/src/main/java/com/example/rickyandmorty/data/uix/view/SettingsScreen.kt
+++ app/src/main/java/com/example/rickyandmorty/data/uix/view/SettingsScreen.kt
@@ -1,5 +1,6 @@
 package com.example.rickyandmorty.data.uix.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -27,6 +29,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.rickyandmorty.data.uix.uicomponent.BottomNavigationBar
@@ -137,4 +141,17 @@ fun LanguageSelection(selectedLanguage: String, onLanguageSelected: (String) -> 
             }
         }
     }
+}
+
+
+@Composable
+fun SocialMediaIcon(iconRes: Int, description: String, onClick: () -> Unit) {
+    Image(
+        painter = painterResource(id = iconRes),
+        contentDescription = description,
+        contentScale = ContentScale.Fit,
+        modifier = Modifier
+            .size(48.dp)
+            .clickable { onClick() }
+    )
 }
